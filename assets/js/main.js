9b154ae5ae70ff7acdac27a16a3e38f369a0568f
Guillermo Barila <gui365@yahoo.com.ar>
2019-03-19 15:58:32 -0400

    Cheerio works. Need to work on ingredients loop

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,24 @@
 window.onload = function() {
+
+  // Event listener on the "Get Recipe" button
   document.getElementById("get-recipe").addEventListener("click", function(){
     event.preventDefault();
     let link = document.getElementById("recipe-link").value;
-    console.log(link);
-    
+    // console.log(link);
+        
+    const params = {
+      body: JSON.stringify({ recipeLink: link }),
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      method: "POST"
+    }
+
+    fetch("/recipe", params )
+      // Just making sure the client can receive info from the server
+      // .then(data => data.json())
+      // .then(newData => console.log(newData.link))
+      .catch(error => console.log(error));
   });
 }
--- server.js
+++ server.js
@@ -1,11 +1,17 @@
 // Declaring dependencies
-const cheerio = require("cheerio");
-const path = require("path");
-const express = require("express");
+const cheerio = require("cheerio"),
+      path = require("path"),
+      express = require("express"),
+      axios = require("axios"),
+      bodyParser = require("body-parser");
 
 // Setting up the server with Express
 let app = express();
 let port = 3000;
+
+// Setting up body parser for POST requests
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 // Server listening on port 3000 
 app.listen(port, () => {
@@ -17,5 +23,36 @@ app.use(express.static(path.join(__dirname, "/assets")));
 
 // Set up HTML routes
 app.get("/", function(req, res) {
-  res.sendfile(path.join(__dirname, "index.html"));
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
+app.post("/recipe", function(req, res) {
+  const link = req.body.recipeLink;
+  
+  // Just making sure the server can send info back to client
+  // console.log(link)
+  res.json({ link });
+
+  // Will use cheerio to scrape NYT Cooking and generate the PDF of the recipe
+  axios.get(link).then(function(response) {
+    const $ = cheerio.load(response.data);
+    
+    let title = $(".recipe-title").text().trim();
+    let author = $(".byline-name").text().trim();
+    let recipeYield = $(".recipe-time-yield").children().first().find(".recipe-yield-value").text().trim();
+    let recipeTime = $(".recipe-time-yield").children().last().find(".recipe-yield-value").text().trim();
+    let intro = $(".topnote").children().first().text().trim();
+    
+    let ingredients = [];
+    $(".recipe-ingredients").each(function(i, ingredient) {
+      // console.log($(ingredient).children().first().text().trim());
+      let ing;
+      $(ingredient).each(function(j, pieceOfInfo) {
+        ing += $(pieceOfInfo).text().replace(/[\n\t\r]/g,"") + " ";
+      })
+      ingredients.push(ing);
+    })
+    console.log(ingredients.length);
+    
+  });
 });
